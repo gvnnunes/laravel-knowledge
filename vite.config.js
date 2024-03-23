5a769e74a013f580e64ad86214f8c5508239fe66
gvnnunes <giovaninunes99@gmail.com>
2024-03-23 19:42:33 -0300

    Installing vue3 and inertiajs packages, configuring vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@Components": path.resolve(__dirname, "resources/js/Components"),
+            "@Pages": path.resolve(__dirname, "resources/js/Pages"),
+        },
+    },
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
